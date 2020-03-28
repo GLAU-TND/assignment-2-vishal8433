9b4d9c0362c4b6fcaab4c75df7425cf5cadcdf8e
vishal jain <vishal.jain_cs18@gla.ac.in>
2020-03-28 16:04:08 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,23 @@
 package problem1.main;
 // executable class
 
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyQueue preQueue = new MyQueue();
+        MyQueue postQueue = new MyQueue();
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree(preQueue, postQueue);
+        myBinarySearchTree.insert(15);
+        myBinarySearchTree.insert(10);
+        myBinarySearchTree.insert(50);
+        myBinarySearchTree.insert(55);
+        myBinarySearchTree.insert(60);
+        myBinarySearchTree.postOrder(myBinarySearchTree.getRoot());
+        myBinarySearchTree.preOrder(myBinarySearchTree.getRoot());
+        System.out.println("PreOrder:");
+        preQueue.show();
+        System.out.println("PostOrder:");
+        postQueue.show();
+        System.out.println();
+    }
 }
