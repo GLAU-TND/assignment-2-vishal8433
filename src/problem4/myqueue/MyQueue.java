6bf7dfb06d77e3adf27894fcf9edcfab726d901b
vishal jain <vishal.jain_cs18@gla.ac.in>
2020-03-28 16:30:58 +0530

    Update MyQueue.java

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -1,11 +1,22 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:11 PM
- */
-package problem4.myqueue;
-// to create queue to store pre - order successor
+package problem4.main;
 
-public class MyQueue {
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
+// executable class
+// use problem1.mybst.MyBinarySearchTree class for creating binary tree
+public class MyMain {
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree (myQueue);
+        myBinarySearchTree.insert(05);
+        myBinarySearchTree.insert(10);
+        myBinarySearchTree.insert(25);
+        myBinarySearchTree.insert(33);
+        myBinarySearchTree.insert(39);
+        myBinarySearchTree.insert(50);
+        myBinarySearchTree.preOrder(myBinarySearchTree.getRoot());
+        System.out.println("PreOrder successors of all the nodes");
+        myQueue.show();
+    }
 }
